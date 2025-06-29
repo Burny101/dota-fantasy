21d36c21849348684f6a4ef72f7f1983b5470182
Bernhard Schenkermayr <ds221011@fhstp.ac.at>
2025-06-29 21:27:49 +0200

    Clean up the code

--- data_ingest_turnaments.py
+++ data_ingest_turnaments.py
@@ -8,19 +8,17 @@ import time
 
 def read_tournament_ids_from_csv(file_path: str):
 	# Reads a CSV file containing tournament data and extracts tournament IDs.
-
 	if not os.path.exists(file_path):
 		raise FileNotFoundError(f"The file {file_path} does not exist.")
-	
 	df = pd.read_csv(file_path, delimiter=';')
-	
 	if 'ID' not in df.columns:
 		raise ValueError("The CSV file must contain a 'ID' column.")
 
 	return df
 
 
-def read_match_ids_from_json(file_path: str):
+def read_match_ids_from_json(file_path: str): # Redunant due to changed workflow
+	# Reads a JSON file containing match data and extracts match IDs.
 	with open(file_path, 'r') as file:
 		data = json.load(file)
 	# Extract only the 'match_id' entries
@@ -31,6 +29,7 @@ def read_match_ids_from_json(file_path: str):
 
 
 def get_match_info(match_ids: list):
+	# Fetches match information from the OpenDota API for a list of match IDs.
 	api_base_url = 'https://api.opendota.com/api/matches/'
 	df_matches = pd.DataFrame()
 	with tqdm(total=len(match_ids)) as pbar:
@@ -45,8 +44,11 @@ def get_match_info(match_ids: list):
 	return df_matches
 
 
-def save_df_to_json(df_to_save: pd.DataFrame, filename: str):
-	df_to_save.to_json("data/json_saves_"+filename+".json", orient='records', lines=True)
+def save_df_to_json(df_to_save: pd.DataFrame, filepath: str, filename: str):
+# Saves a DataFrame to a JSON file in the specified directory. Creates the directory if it does not exist.
+	if not os.path.exists(filepath):
+		os.makedirs(filepath)
+	df_to_save.to_json(filepath+filename+".json", orient='records', lines=True)
 
 
 def get_all_matches_from_tournaments(tournaments: pd.DataFrame):
@@ -69,6 +71,9 @@ tournaments = read_tournament_ids_from_csv(file_path='data/turniere.csv')
 match_ids = get_all_matches_from_tournaments(tournaments)
 print(match_ids)
 big_df = get_match_info(match_ids)
-save_df_to_json(big_df, "all_matches_from_tournaments")
+
+# Save all matches to a JSON file
+matches_path = "data/matches/"
+save_df_to_json(matches_path, big_df, "all_matches_from_tournaments")
 
 print("Script ran successfully")
