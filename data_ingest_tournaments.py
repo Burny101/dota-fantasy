da1d179858fdda3e2f8c549383649915b6a31c38
Bernhard Schenkermayr <ds221011@fhstp.ac.at>
2025-07-19 19:44:32 +0200

    Adapt script to put date variable through

--- data_ingest_tournaments.py
+++ data_ingest_tournaments.py
@@ -28,16 +28,19 @@ def read_match_ids_from_json(file_path: str): # Redunant due to changed workflow
 	return df
 
 
-def get_match_info(match_ids: list):
-	# Fetches match information from the OpenDota API for a list of match IDs.
+def get_match_info(match_ids: pd.DataFrame):
+	# Fetches match information from the OpenDota API for a DataFrame of match IDs and start_dates.
 	api_base_url = 'https://api.opendota.com/api/matches/'
 	df_matches = pd.DataFrame()
 	with tqdm(total=len(match_ids)) as pbar:
-		for match_id in match_ids:
+		for idx, row in match_ids.iterrows():
+			match_id = row['match_id']
+			start_date = row['tournament_start_date']
 			response = requests.get(api_base_url + str(match_id))
 			time.sleep(1)
 			if response.status_code == 200:
 				match_data = json.loads(response.text)
+				match_data['tournament_start_date'] = start_date
 				df_row = pd.DataFrame([match_data])
 				df_matches = pd.concat([df_matches, df_row], ignore_index=True)
 			pbar.update(1)
@@ -52,28 +55,33 @@ def save_df_to_json(df_to_save: pd.DataFrame, filepath: str, filename: str):
 
 
 def get_all_matches_from_tournaments(tournaments: pd.DataFrame):
-	# create a JSON file with all the matches played in all the tournaments
-	
-	all_match_ids = []
+	# Create a DataFrame with all the matches played in all the tournaments, including start_date
+	all_matches = []
 	for index, row in tqdm(tournaments.iterrows(), total=tournaments.shape[0]):
 		tournament_id = row['ID']
+		start_date = row['Date First Game']
 		api_url = f'https://api.opendota.com/api/leagues/{tournament_id}/matches'
 		response = requests.get(api_url)
 		time.sleep(1)
 		if response.status_code == 200:
 			matches = json.loads(response.text)
-			match_ids = [match['match_id'] for match in matches]
-			all_match_ids.extend(match_ids)
-	return all_match_ids
+			for match in matches:
+				all_matches.append({
+					'match_id': match['match_id'],
+					'tournament_start_date': start_date
+				})
+	df_all_matches = pd.DataFrame(all_matches)
+	return df_all_matches
 	
 
 tournaments = read_tournament_ids_from_csv(file_path='data/turniere.csv')
+print(tournaments)
 match_ids = get_all_matches_from_tournaments(tournaments)
 #print(match_ids)
 big_df = get_match_info(match_ids)
 
 # Save all matches to a JSON file
 matches_path = "data/matches/"
-save_df_to_json(matches_path, big_df, "all_matches_from_tournaments")
+save_df_to_json(big_df, matches_path, "all_matches_from_tournaments")
 
 print("Script ran successfully!")
